4cbd4ec45e49926da36d583e3d391feb46a714d2
Martin Davy <martin.davy@gmail.com>
2024-01-08 15:57:11 -0500

    bug fixes and styling

--- lib/widgets/song_item.dart
+++ lib/widgets/song_item.dart
@@ -12,24 +12,26 @@ class SongItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: SongPositionIndicator(position, song),
-        title: Text(song.songName),
-        subtitle: Text(song.artist),
-        trailing: song.videoId.isEmpty ? null
-            : IconButton(
-                icon: const Icon(Icons.video_library, color: Colors.red,),
-                onPressed: () {
-                  var uri = Uri(
-                    scheme: 'https',
-                    host: 'www.youtube.com',
-                    path: 'watch',
-                    queryParameters: {'v': song.videoId},
-                  );
-                  launchUrl(uri);
-                },
-              ),
-      ),
+          child: ListTile(
+            leading: SongPositionIndicator(position, song),
+            title: Text(
+            song.songName,
+            style: Theme.of(context).textTheme.titleMedium),
+            subtitle: Text(song.artist),
+            trailing: song.videoId.isEmpty ? null
+                : IconButton(
+                    icon: const Icon(Icons.video_library, color: Colors.red,),
+                    onPressed: () {
+                      var uri = Uri(
+                        scheme: 'https',
+                        host: 'www.youtube.com',
+                        path: 'watch',
+                        queryParameters: {'v': song.videoId},
+                      );
+                      launchUrl(uri);
+                    },
+                  ),
+          ),
     );
   }
 }
